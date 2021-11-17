8d3205216bca8d4fd014194d74780e1595b3a7ff
McKayla Lankau <lankaukk@yahoo.com>
2021-11-17 18:35:01 -0500

    stubbed out structure

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,10 @@ import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
-    const [click, setClick] = useState(false)
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
     return (
         <>
             <nav className="navbar">
@@ -10,9 +13,31 @@ function Navbar() {
                     <Link to="/" className="navbar-logo">
                         MCKAY <i className="fab fa-typo3" />
                     </Link>
-                    <div className="menu-icon">
+                    <div className="menu-icon" onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                     </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to="/services" className="nav-links" onClick={closeMobileMenu}>
+                                Services
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to="/products" className="nav-links" onClick={closeMobileMenu}>
+                                Products
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to="/sign-up" className="nav-links-mobile" onClick={closeMobileMenu}>
+                                Sign Up
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </>
