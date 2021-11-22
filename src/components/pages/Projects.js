fe1391e788cf380e77dd2676676d70a248b8b412
McKayla Lankau <lankaukk@yahoo.com>
2021-11-22 10:24:31 -0500

    added footer pages

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import  '../../App.css'
+import React from 'react';
+import '../../App.css';
 
 export default function Projects() {
     return <h1 className="projects">PROJECTS</h1>
