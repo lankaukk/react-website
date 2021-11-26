02203bf67ba551ed3047476f4bc325e69d0b1cca
McKayla Lankau <lankaukk@yahoo.com>
2021-11-26 17:18:32 -0500

    color adjustments

--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import '../../App.css'
 import Footer from '../Footer'
-import Cards from '../Cards'
 
 function Gallery() {
     return (
         <>
             <br></br><br></br>
             <h1>Gallery</h1>
-            <Cards />
+            
             <Footer />
         </>
     )
