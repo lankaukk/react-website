3e0a5be61cfa78f6c1f7d69f61280fd17c18d6d4
McKayla Lankau <lankaukk@yahoo.com>
2021-11-28 17:31:38 -0500

    created css animation heropage

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,8 +7,18 @@ function HeroSection() {
     return (
         <div className='hero-container'>
             <video src="" autoPlay loop muted />
-            <h1>Developer + Designer</h1>
+            <h2>Web Developer + Designer</h2>
             
+            <div className="circle c1"></div>
+            <div className="circle c2"></div>
+            <div className="circle c3"></div>
+
+            <div className="line vertical l1"></div>
+            <div className="line vertical l2"></div>
+            <div className="line vertical l3"></div>
+
+            <div className="line horizontal l4"></div>
+            <div className="line horizontal l5"></div>
         </div>
     )
 }
