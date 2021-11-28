b86da056e260df18b03d924ef130e3aff31a2e8c
McKayla Lankau <lankaukk@yahoo.com>
2021-11-28 16:42:38 -0500

    edited gallery images

--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -11,44 +11,42 @@ function Gallery() {
             {/* <h1>Gallery</h1> */}
 
             <div class="row"> 
+
                 <div class="column">
-                    <img src="images/_blank-playspace.gif" width="100%"></img>
-                    <img src="images/journal-mockup.jpg" width="100%" ></img>
-                    <img src="images/typo-posters.jpg" width="100%"></img>
-                    <img src="images/pigeon-square.png" width="100%"></img>
-                    <img src="images/_blank-playspace.gif" width="100%"></img>
-                    <img src="images/journal-mockup.jpg" width="100%"></img>
-                    <img src="images/typo-posters.jpg" width="100%"></img>
-                </div>
-                <div class="column">
+                    <img src="images/gallery/birth-of-the-moon.gif" width="100%" alt="gallery image"></img>
+                    <img src="images/gallery/earthjuice.jpg" width="100%" alt="gallery image" ></img>
+                    <img src="images/gallery/marsjuice.jpg" width="100%" alt="gallery image"></img>
+                    <img src="images/gallery/moonscape.jpg" width="100%" alt="gallery image"></img>
+                    <img src="images/gallery/p9.jpg" width="100%" alt="gallery image"></img>
+                    <img src="images/gallery/p14.jpg" width="100%" alt="gallery image"></img>
                     
-                    <img src="images/journal-mockup.jpg" width="100%" ></img>
-                    <img src="images/typo-posters.jpg" width="100%"></img>
-                    <img src="images/pigeon-square.png" width="100%"></img>
-                    <img src="images/_blank-playspace.gif" width="100%"></img>
-                    <img src="images/journal-mockup.jpg" width="100%"></img>
-                    <img src="images/typo-posters.jpg" width="100%"></img>
-                    <img src="images/_blank-playspace.gif" width="100%"></img>
                 </div>
+
                 <div class="column">
-                    
-                    <img src="images/typo-posters.jpg" width="100%"></img>
-                    <img src="images/pigeon-square.png" width="100%"></img>
-                    <img src="images/_blank-playspace.gif" width="100%"></img>
-                    <img src="images/journal-mockup.jpg" width="100%"></img>
-                    <img src="images/typo-posters.jpg" width="100%"></img>
-                    <img src="images/_blank-playspace.gif" width="100%"></img>
-                    <img src="images/journal-mockup.jpg" width="100%" ></img>
+                    <img src="images/gallery/p22.jpg" width="100%" alt="gallery image" ></img>
+                    <img src="images/gallery/cloud-sticker.jpg" width="100%" alt="gallery image"></img>
+                    <img src="images/gallery/leafcompillation.jpg" width="100%" alt="gallery image"></img>
+                    <img src="images/gallery/p17.jpg" width="100%" alt="gallery image"></img>
+                    <img src="images/_blank-playspace.gif" width="100%" alt="gallery image"></img>
+                    <img src="images/journal-mockup.jpg" width="100%" alt="gallery image"></img>
                 </div>
+
                 <div class="column">
-                    
-                    <img src="images/pigeon-square.png" width="100%"></img>
-                    <img src="images/_blank-playspace.gif" width="100%"></img>
-                    <img src="images/journal-mockup.jpg" width="100%"></img>
-                    <img src="images/typo-posters.jpg" width="100%"></img>
-                    <img src="images/_blank-playspace.gif" width="100%"></img>
-                    <img src="images/journal-mockup.jpg" width="100%" ></img>
-                    <img src="images/typo-posters.jpg" width="100%"></img>
+                    <img src="images/typo-posters.jpg" width="100%" alt="gallery image"></img>
+                    <img src="images/pigeon-square.png" width="100%" alt="gallery image"></img>
+                    <img src="images/_blank-playspace.gif" width="100%" alt="gallery image"></img>
+                    <img src="images/journal-mockup.jpg" width="100%" alt="gallery image"></img>
+                    <img src="images/typo-posters.jpg" width="100%" alt="gallery image"></img>
+                    <img src="images/_blank-playspace.gif" width="100%" alt="gallery image"></img>
+                </div>
+
+                <div class="column">
+                    <img src="images/pigeon-square.png" width="100%" alt="gallery image"></img>
+                    <img src="images/_blank-playspace.gif" width="100%" alt="gallery image"></img>
+                    <img src="images/journal-mockup.jpg" width="100%" alt="gallery image"></img>
+                    <img src="images/typo-posters.jpg" width="100%" alt="gallery image"></img>
+                    <img src="images/_blank-playspace.gif" width="100%" alt="gallery image"></img>
+                    <img src="images/journal-mockup.jpg" width="100%" alt="gallery image" ></img>
                 </div>
             </div>
 
