1f4344e7e9e14c7b9c02d5be39153f225da0f2ae
McKayla Lankau <lankaukk@yahoo.com>
2021-12-01 22:35:38 -0500

    Project Show Component and first page props

--- src/components/pages/projects/ProjectShow.js
+++ src/components/pages/projects/ProjectShow.js
@@ -13,8 +13,6 @@ function ProjectShow(props) {
                 </a>
             </p>
 
-            
-            
             <img
                 className='project-show-pic'
                 alt='Portfolio Image'
@@ -23,16 +21,54 @@ function ProjectShow(props) {
 
             <p className="project-show-description">{props.description}</p>
 
+            <p className="project-show-description">{props.label2}</p>
             <img
                 className='project-show-pic'
                 alt='Portfolio Image'
                 src={props.src2}
             />
+            <p className="project-show-description">{props.label3}</p>
              <img
                 className='project-show-pic'
                 alt='Portfolio Image'
                 src={props.src3}
             />
+
+            <br></br>
+
+            <p className="project-show-description">{props.label4}</p>
+            <iframe 
+                className='project-show-pic' 
+                src={props.src4}
+                alt='Portfolio Video'
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen>
+            </iframe>
+
+            <br></br>
+
+            <p className="project-show-description">{props.label5}</p>
+            <div className="project-show-pics-container">
+                <img
+                    className='project-show-pics'
+                    alt='Portfolio Image'
+                    src={props.src5}
+                />
+                <img
+                    className='project-show-pics'
+                    alt='Portfolio Image'
+                    src={props.src6}
+                />
+                <img
+                    className='project-show-pics'
+                    alt='Portfolio Image'
+                    src={props.src7}
+                />
+            </div>
+            
+            <br></br><br></br><br></br><br></br>
 
             <p className="project-show-description ">{props.date}</p>
 
--- src/components/pages/projects/Typo.js
+++ src/components/pages/projects/Typo.js
@@ -8,12 +8,26 @@ function Typo() {
             
             <ProjectShow 
                 title="Typo"
-                src='/images/typo-posters.jpg'
-                description="Typo is a web application for generating your own digital typographic compositions. There is a gallery where you can save your favorite works, and view those created by others. The frontend is built with HTML, CSS, and JavaScript, and it communicates with a backend API that is built with Ruby and Rails.
-                "
-                src2='/images/typo-posters.jpg'
-                src3='/images/typo-posters.jpg'
                 gh='https://github.com/lankaukk/typo-app'
+
+                src='/images/typo-posters.jpg'
+
+                description="Typo is a web application for generating your own digital typographic compositions. There is a gallery where you can save your favorite works, and view those created by others. The frontend is built with HTML, CSS, and JavaScript, and it communicates with a backend API that is built with Ruby and Rails."
+
+                label2="Interface Design:"
+                src2='/images/projects/typo/typo-interface-1.png'
+                label3=''
+                src3='/images/projects/typo/typo-interface-2.png'
+
+                label4='Walkthrough Video:'
+                src4='https://www.youtube.com/embed/1N06yzaWQY8'
+                
+                label5='Download your compositions and print them out!'
+                src5='/images/projects/typo/typo-printing.gif'
+                src6='/images/projects/typo/typo-poster-1.jpg'
+                src7='/images/projects/typo/typo-posters-angle.jpg'
+
+                
                 date="Fall, 2021"
             />
         </div>
