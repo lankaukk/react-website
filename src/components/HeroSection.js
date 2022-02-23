9ce11d801a2548ed0426e0f3268184f4037f441c
McKayla Lankau <lankaukk@yahoo.com>
2022-02-22 23:44:42 -0500

    animated  hhero adjective bc i cantt llimitt myself to one adjective

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,11 +7,20 @@ class HeroSection extends Component {
 
     
     componentDidMount() {
-        console.log('componentDidMount() lifecycle');
+        const text0 = ["Web", "UI", "Digital", "UX"];
+        let elem0 = document.getElementById("adjectives");
+        let mycounter = 0;
+        setInterval(change2, 4000);
+        function change2() {
+        elem0.innerHTML = text0[mycounter];
+            mycounter++;
+            if(mycounter >= text0.length) { mycounter = 0; }
+        }
+        
+        
         const text = ["Designer", "Developer", "Artist", "Engineer"];
-        let counter = 0;
         let elem = document.getElementById("professions");
-        console.log(elem);
+        let counter = 0;
         setInterval(change, 1000);
         function change() {
         elem.innerHTML = text[counter];
@@ -26,7 +35,8 @@ class HeroSection extends Component {
                 <div className='bigcirclecontainer'>
                     <div className="circle c0"></div>
                     <h1 style={{zIndex: '10',  fontSize: '10vw'}}>
-                        Web<br></br>
+                        <div id="adjectives">Web</div>
+                        
                         <div id="professions">Designer</div>
                     </h1>
                 </div>
