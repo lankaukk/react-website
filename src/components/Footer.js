ba4897da9bbae4d9513f34083be68999595fc2cb
McKayla Lankau <lankaukk@yahoo.com>
2022-07-08 00:31:17 -0400

    reorganized home page, responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,40 +1,38 @@
-import React from 'react';
-import './Footer.css';
-import { Link } from 'react-router-dom';
-import { ExternalLink } from 'react-external-link';
+import React from "react";
+import "./Footer.css";
+import { Link } from "react-router-dom";
+import { ExternalLink } from "react-external-link";
 
 function Footer() {
   return (
-    <div className='footer-container'>
-      <section class='social-media'>
-        <div class='social-media-wrap'>
-          <div class='footer-logo'>
-            <Link to='/' className='social-logo'>
-              MCKAYLA LANKAU
-              
-            </Link>
-          </div>
-          <small class='website-rights'>MCKAYLA LANKAU © 2022</small>
-          <div class='social-icons'>
-            
-            <ExternalLink class='social-icon-link instagram' href="https://www.instagram.com/forwardchaos/">
-                <i class='fab fa-instagram' />
-            </ExternalLink>
+    <div className="footer-container">
+      <small class="website-rights">MCKAYLA LANKAU © 2022</small>
+      <div class="social-icons">
+        <ExternalLink
+          class="social-icon-link instagram"
+          href="https://www.instagram.com/forwardchaos/"
+        >
+          <i class="fab fa-instagram" />
+        </ExternalLink>
 
-            {/* <ExternalLink class='social-icon-link youtube' href="https://www.youtube.com/channel/UCmbbTQKwmwz_5rw6V9n-15g/featured">
+        {/* <ExternalLink class='social-icon-link youtube' href="https://www.youtube.com/channel/UCmbbTQKwmwz_5rw6V9n-15g/featured">
                 <i class='fab fa-youtube' />
             </ExternalLink> */}
-            
-            <ExternalLink class='social-icon-link linkedin' href="https://www.linkedin.com/in/mckayla-lankau/">
-                <i class='fab fa-linkedin' />
-            </ExternalLink>
 
-            <ExternalLink class='social-icon-link github' href="https://github.com/lankaukk">
-                <i class='fab fa-github' />
-            </ExternalLink>
-          </div>
-        </div>
-      </section>
+        <ExternalLink
+          class="social-icon-link linkedin"
+          href="https://www.linkedin.com/in/mckayla-lankau/"
+        >
+          <i class="fab fa-linkedin" />
+        </ExternalLink>
+
+        <ExternalLink
+          class="social-icon-link github"
+          href="https://github.com/lankaukk"
+        >
+          <i class="fab fa-github" />
+        </ExternalLink>
+      </div>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,68 +1,72 @@
-import React, {useState, useEffect} from 'react'
-import { Link } from 'react-router-dom'
-import { Button } from './Button';
-import './Navbar.css';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Button } from "./Button";
+import "./Navbar.css";
 
 function Navbar() {
-    const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
-    const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
-    const showButton = () => {
-        if(window.innerWidth <= 960) {
-            setButton(false)
-        } else {
-            setButton(true)
-        }
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
     }
+  };
 
-    useEffect(() => {
-        showButton();
-    }, []);
+  useEffect(() => {
+    showButton();
+  }, []);
 
-    window.addEventListener('resize', showButton);
+  window.addEventListener("resize", showButton);
 
-    return (
-        <>
-            <nav className="navbar">
-                <div className="navbar-container">
-                
-                
-                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-                        MCKAYLA LANKAU 
-                    </Link>
-                    <div className="menu-icon" onClick={handleClick}>
-                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-                    </div>
-                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                        <li className='nav-item'>
-                            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                                Home
-                            </Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to="/projects" className="nav-links" onClick={closeMobileMenu}>
-                                Projects
-                            </Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to="/gallery" className="nav-links" onClick={closeMobileMenu}>
-                                Gallery
-                            </Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to="/contact" className="nav-links-mobile" onClick={closeMobileMenu}>
-                                Contact
-                            </Link>
-                        </li>
-                    </ul>
-                    {button && <Button buttonStyle='btn--outline'>Contact</Button>}
-                </div>
-            </nav>
-        </>
-    )
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+          MCKAYLA LANKAU
+        </Link>
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"} />
+        </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/projects"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              Projects
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/gallery" className="nav-links" onClick={closeMobileMenu}>
+              Gallery
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/contact"
+              className="nav-links-mobile"
+              onClick={closeMobileMenu}
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+        {button && <Button buttonStyle="btn--outline">Contact</Button>}
+      </nav>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
