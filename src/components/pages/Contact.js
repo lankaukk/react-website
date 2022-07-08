b77d4c32d796521b78e1ae64f317309c2cc8d03c
McKayla Lankau <lankaukk@yahoo.com>
2022-07-08 01:23:51 -0400

    cleaned up contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,132 +1,111 @@
-import React from 'react'
-import '../../Contact.css'
+import React from "react";
+import "../../Contact.css";
 
-function Contact() {
-    return (
-        <>
-        <div class="contact-container">
-        <br></br>
-            <h1 className="hi">Hi! My name's McKayla :)</h1>
-            <br></br>
-            <img
-                class='pro-pic'
-                src='images/pro-pic.jpg'
-                alt='selfie'
-            />
-            
-            <p class="contact-description">I'm a Communication Designer and Web Developer from New York. Passionate about interdisciplinary collaboration, and creating meaningful digital products.</p>
-
-            
-            <p class="contact-description">
-            Email me at <a href="mailto: mckaylalankau@gmail.com">(mckaylalankau@gmail.com)</a> or message me on <a href="https://www.linkedin.com/in/mckayla-lankau/">LinkedIn</a> for Project Comissions and Collaborations. 👩‍💻
-            </p>
-
-            <p class="contact-description">
-            
-            </p>
-
-            <div className="sticker-container">
-                <img
-                    class='sticker-item delay0'
-                    src='images/stickers/ai.png'
-                    alt='sticker'
-                />
-                <img
-                    class='sticker-item delay1'
-                    src='images/stickers/ps.png'
-                    alt='sticker'
-                />
-                <img
-                    class='sticker-item delay2'
-                    src='images/stickers/indesign.png'
-                    alt='sticker'
-                />
-                <img
-                    class='sticker-item delay3'
-                    src='images/stickers/adobecc.png'
-                    alt='sticker'
-                />
-                <img
-                    class='sticker-item delay4'
-                    src='images/stickers/figma.png'
-                    alt='sticker'
-                />
-                <img
-                    class='sticker-item delay5'
-                    src='images/stickers/github.png'
-                    alt='sticker'
-                />
-                <img
-                    class='sticker-item delay4'
-                    src='images/stickers/git.png'
-                    alt='sticker'
-                />
-                <img
-                    class='sticker-item delay3'
-                    src='images/stickers/js.png'
-                    alt='sticker'
-                />
-                {/* <img
-                    class='sticker-item delay2'
-                    src='images/stickers/slack.png'
-                    alt='sticker'
-                /> */}
-                <img
-                    class='sticker-item delay1'
-                    src='images/stickers/vscode.png'
-                    alt='sticker'
-                />
-                <img
-                    class='sticker-item delay0'
-                    src='images/stickers/ruby.png'
-                    alt='sticker'
-                />
-                <img
-                    class='sticker-item delay1'
-                    src='images/stickers/rails.png'
-                    alt='sticker'
-                />
-                <img
-                    class='sticker-item delay2'
-                    src='images/stickers/jquery.png'
-                    alt='sticker'
-                />
-                <img
-                    class='sticker-item delay3'
-                    src='images/stickers/react.png'
-                    alt='sticker'
-                />
-                <img
-                    class='sticker-item delay4'
-                    src='images/stickers/redux.png'
-                    alt='sticker'
-                />
-                
-                <img
-                    class='sticker-item delay5'
-                    src='images/stickers/html.png'
-                    alt='sticker'
-                />
-                <img
-                    class='sticker-item delay4'
-                    src='images/stickers/css.png'
-                    alt='sticker'
-                />
-                <img
-                    class='sticker-item delay3'
-                    src='images/stickers/scss.png'
-                    alt='sticker'
-                />
-            </div>
-            
-            
-
-            <br></br><br></br>
-
-            
+export default function Contact() {
+  return (
+    <>
+      <div className="contact-container">
+        <div className="contact-heading">
+          <h1 className="hi">Hi! I'm McKayla 👩‍💻</h1>
+          <img class="pro-pic" src="images/pro-pic.jpg" alt="selfie" />
         </div>
-        </>
-    )
-}
 
-export default Contact;
+        <div class="contact-description">
+          If you would like your own website like this, you're interested in
+          collaborating, or you just want to chat about design and web development...
+          ☕<br></br> <br></br>
+          Email me at{" "}
+          <a href="mailto: mckaylalankau@gmail.com">
+            mckaylalankau@gmail.com
+          </a>{" "}
+          <br></br>
+          Or DM me on{" "}
+          <a href="https://www.linkedin.com/in/mckayla-lankau/">LinkedIn</a>
+        </div>
+      </div>
+      <div className="sticker-container">
+        <img
+          class="sticker-item delay0"
+          src="images/stickers/ai.png"
+          alt="sticker"
+        />
+        <img
+          class="sticker-item delay1"
+          src="images/stickers/ps.png"
+          alt="sticker"
+        />
+        <img
+          class="sticker-item delay2"
+          src="images/stickers/indesign.png"
+          alt="sticker"
+        />
+        <img
+          class="sticker-item delay4"
+          src="images/stickers/figma.png"
+          alt="sticker"
+        />
+        <img
+          class="sticker-item delay5"
+          src="images/stickers/github.png"
+          alt="sticker"
+        />
+        <img
+          class="sticker-item delay4"
+          src="images/stickers/git.png"
+          alt="sticker"
+        />
+        <img
+          class="sticker-item delay3"
+          src="images/stickers/js.png"
+          alt="sticker"
+        />
+        <img
+          class="sticker-item delay1"
+          src="images/stickers/vscode.png"
+          alt="sticker"
+        />
+        <img
+          class="sticker-item delay0"
+          src="images/stickers/ruby.png"
+          alt="sticker"
+        />
+        <img
+          class="sticker-item delay1"
+          src="images/stickers/rails.png"
+          alt="sticker"
+        />
+        <img
+          class="sticker-item delay2"
+          src="images/stickers/jquery.png"
+          alt="sticker"
+        />
+        <img
+          class="sticker-item delay3"
+          src="images/stickers/react.png"
+          alt="sticker"
+        />
+        <img
+          class="sticker-item delay4"
+          src="images/stickers/redux.png"
+          alt="sticker"
+        />
+
+        <img
+          class="sticker-item delay5"
+          src="images/stickers/html.png"
+          alt="sticker"
+        />
+        <img
+          class="sticker-item delay4"
+          src="images/stickers/css.png"
+          alt="sticker"
+        />
+        <img
+          class="sticker-item delay3"
+          src="images/stickers/scss.png"
+          alt="sticker"
+        />
+      </div>
+    </>
+  );
+}
