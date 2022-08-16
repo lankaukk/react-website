d9fe6ec152f511bf4d1af32a41220a9a829bab3a
McKayla Lankau <lankaukk@yahoo.com>
2022-08-15 23:16:34 -0400

    fetching gallery artwork from api now yay

---
+++ src/components/gallerycard.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export var GalleryCard = (props) => {
+  return (
+    <div
+      style={{
+        width: "20vh",
+      }}
+    >
+      <img src={props.imageUrl}  alt="gallery item" style={{width: '100%'}}></img>
+      <div
+        style={{
+          padding: 15,
+          display: "flex",
+          flexDirection: "column",
+          gap: ".3em",
+          color: "rgb(28, 28, 28)",
+          background: "white",
+        }}
+      >
+        <div>{props.title}</div>
+        <div style={{ fontSize: ".7em" }}>
+          {props.medium}, {props.date}
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -1,59 +1,44 @@
-import React from 'react'
-import '../../Gallery.css'
+import React from "react";
+import "../../Gallery.css";
+import { GalleryCard } from "../gallerycard.js";
+import { useState, useEffect } from "react";
 
 function Gallery() {
-    return (
-        <>
-            <div className="gallery-container">
-            <div class="row"> 
+  let PROJECT_ID = "j1fgz8za";
+  let DATASET = "production";
+  let QUERY = encodeURIComponent('*[_type == "artwork"]');
 
-            {/* graphic posters */}
-                <div class="column">
-                    <img src="images/gallery/birth-of-the-moon.gif" width="100%" alt="gallery item"></img>
-                    <img src="images/gallery/plutojuice.jpg" width="100%" alt="gallery item" ></img>
-                    <img src="images/gallery/earthjuice.jpg" width="100%" alt="gallery item" ></img>
-                    <img src="images/gallery/marsjuice.jpg" width="100%" alt="gallery item"></img>
-                    
-                    <img src="images/gallery/escape.jpg" width="100%" alt="gallery item"></img>
-                </div>
+  let URL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`;
 
-            {/* mixed media */}
-                <div class="column">
-                    <img src="images/gallery/you-are-safe.jpg" width="100%" alt="gallery item" ></img>
-                    <img src="images/gallery/cloud-sticker.jpg" width="100%" alt="gallery item"></img>
-                    <img src="images/gallery/leafcompillation.jpg" width="100%" alt="gallery item"></img>
-                    <img src="images/gallery/chaos-smudged.gif" width="100%" alt="gallery item"></img>
-                    <img src="images/gallery/forward.jpg" width="100%" alt="gallery item"></img>
+  const [artworks, setArtworks] = useState([]);
 
-                    <img src="images/gallery/ui-study.jpg" width="100%" alt="gallery item"></img>
-                </div>
+  useEffect(() => {
+    fetch(URL)
+      .then((res) => res.json())
+      .then(({ result }) => {
+        if (result.length > 0) {
+          result.forEach((artwork) => {
+            setArtworks((oldArray) => [...oldArray, artwork]);
+          });
+        }
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
-
-                <div class="column">
-                    <img src="images/gallery/spinning-circles.gif" width="100%" alt="gallery item"></img>
-                    <img src="images/gallery/poster-1.1.jpg" width="100%" alt="gallery item"></img>
-                    <img src="images/gallery/poster-1.3.jpg" width="100%" alt="gallery item"></img>
-            
-                    <img src="images/gallery/gems.jpg" width="100%" alt="gallery item"></img>
-                    <img src="images/gallery/simulationplayground.jpg" width="100%" alt="gallery item"></img>
-                    
-                </div>
-
-            {/* landscapes */}
-                <div class="column">
-                    <img src="images/gallery/fredonia.jpg" width="100%" alt="gallery item"></img>
-                    <img src="images/gallery/moonscape.jpg" width="100%" alt="gallery item"></img>
-                    <img src="images/gallery/oldfield.jpg" width="100%" alt="gallery item"></img>
-                    <img src="images/gallery/greenlawn.jpg" width="100%" alt="gallery item"></img>
-                    <img src="images/gallery/sunflowers.jpg" width="100%" alt="gallery item"></img>
-                    <img src="images/gallery/reilly.jpg" width="100%" alt="gallery item"></img>
-                    <img src="images/gallery/india.jpg" width="100%" alt="gallery item"></img>
-                </div>
-            </div>
-
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="gallery-container">
+        {artworks.map((artwork) => (
+          <GalleryCard
+            title={artwork.title}
+            imageUrl={artwork.imageUrl}
+            date={artwork.date}
+            medium={artwork.medium}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Gallery;
