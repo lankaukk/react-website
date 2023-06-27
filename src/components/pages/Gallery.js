266108300b070c11b3f112fe79e8b12cdee95c4a
McKayla Lankau <lankaukk@yahoo.com>
2023-06-27 00:08:22 -0400

    implement masonry layout to gallery

--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -1,34 +1,18 @@
 import React from "react";
-import "../../Gallery.css";
 import { useState, useEffect, useCallback } from "react";
-import styled from "styled-components";
+import "./masonry.css";
 
 var GalleryCard = (props) => {
   const date = props.date;
   const year = date.slice(0, 4);
 
   return (
-    <div>
-      <div className="card-holder">
-        <img
-          src={props.imageUrl}
-          alt="gallery item"
-          style={{ width: "100%" }}
-        ></img>
-        <div
-          style={{
-            padding: 15,
-            display: "flex",
-            flexDirection: "column",
-            gap: ".3em",
-            color: "black",
-            background: "white",
-          }}
-        >
-          <div style={{ fontSize: ".9em" }}>{props.title}</div>
-          <div style={{ fontSize: ".7em" }}>
-            {props.medium}, {year}
-          </div>
+    <div className="card-holder">
+      <img src={props.imageUrl} alt="gallery item" />
+      <div>
+        <div>{props.title}</div>
+        <div>
+          {props.medium}, {year}
         </div>
       </div>
     </div>
