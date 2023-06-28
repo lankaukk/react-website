05a72338da1207845c0d8f12982a31ba32b54e19
McKayla Lankau <lankaukk@yahoo.com>
2023-06-27 23:46:42 -0400

    fetch artworks on app load

--- src/App.js
+++ src/App.js
@@ -1,44 +1,81 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/pages/Home';
-import Projects from './components/pages/Projects';
-import Gallery from './components/pages/Gallery';
-import Contact from './components/pages/Contact';
-import Footer from './components/Footer'
-import Typo from './components/pages/projects/Typo';
-import JournalAppDesign from './components/pages/projects/JournalAppDesign';
-import ProtestPlatform from './components/pages/projects/ProtestPlatform';
-import NoLikes from './components/pages/projects/NoLikes';
-import ModernPotions from './components/pages/projects/ModernPotions';
-import RobotPigeonAR from './components/pages/projects/RobotPigeonAR';
-import PexelsBrowser from './components/pages/projects/PexelsBrowser';
-import Blank from './components/pages/projects/Blank';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Projects from "./components/pages/Projects";
+import Gallery from "./components/pages/Gallery";
+import Contact from "./components/pages/Contact";
+import Footer from "./components/Footer";
+import Typo from "./components/pages/projects/Typo";
+import JournalAppDesign from "./components/pages/projects/JournalAppDesign";
+import ProtestPlatform from "./components/pages/projects/ProtestPlatform";
+import NoLikes from "./components/pages/projects/NoLikes";
+import ModernPotions from "./components/pages/projects/ModernPotions";
+import RobotPigeonAR from "./components/pages/projects/RobotPigeonAR";
+import PexelsBrowser from "./components/pages/projects/PexelsBrowser";
+import Blank from "./components/pages/projects/Blank";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [artworks, setArtworks] = useState([]);
+
+  useEffect(() => {
+    let PROJECT_ID = "j1fgz8za";
+    let DATASET = "production";
+    let QUERY = encodeURIComponent('*[_type == "artwork"]');
+
+    let URL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`;
+    fetch(URL)
+      .then((res) => res.json())
+      .then(({ result }) => {
+        if (result.length > 0) {
+          result.forEach((artwork) => {
+            setArtworks((oldArray) => [...oldArray, artwork]);
+          });
+        }
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <>
       <Router>
-        <Navbar/>
+        <Navbar />
         <Routes>
           <Route path="/*" element={<Home />}></Route>
           <Route path="/projects" element={<Projects />}></Route>
-            <Route path="/projects/typo" element={<Typo />}></Route>
-            <Route path="/projects/journal-app-design" element={<JournalAppDesign />}></Route>
-            <Route path="/projects/protest-platform" element={<ProtestPlatform />}></Route>
-            <Route path="/projects/_blank" element={<Blank />}></Route>
-            <Route path="/projects/no-likes" element={<NoLikes />}></Route>
-            <Route path="/projects/pexels-browser" element={<PexelsBrowser />}></Route>
-            <Route path="/projects/robot-pigeon-ar" element={<RobotPigeonAR />}></Route>
-            <Route path="/projects/modern-potions" element={<ModernPotions />}></Route>
-          <Route path="/gallery" element={<Gallery />}></Route>
+          <Route path="/projects/typo" element={<Typo />}></Route>
+          <Route
+            path="/projects/journal-app-design"
+            element={<JournalAppDesign />}
+          ></Route>
+          <Route
+            path="/projects/protest-platform"
+            element={<ProtestPlatform />}
+          ></Route>
+          <Route path="/projects/_blank" element={<Blank />}></Route>
+          <Route path="/projects/no-likes" element={<NoLikes />}></Route>
+          <Route
+            path="/projects/pexels-browser"
+            element={<PexelsBrowser />}
+          ></Route>
+          <Route
+            path="/projects/robot-pigeon-ar"
+            element={<RobotPigeonAR />}
+          ></Route>
+          <Route
+            path="/projects/modern-potions"
+            element={<ModernPotions />}
+          ></Route>
+          <Route
+            path="/gallery"
+            element={<Gallery artworks={artworks} />}
+          ></Route>
           <Route path="/contact" element={<Contact />}></Route>
         </Routes>
         <Footer />
       </Router>
-
-      
     </>
   );
 }
--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useState, useEffect } from "react";
 import "./masonry.css";
 
 var GalleryCard = (props) => {
@@ -19,44 +18,23 @@ var GalleryCard = (props) => {
   );
 };
 
-function Gallery() {
-  const [artworks, setArtworks] = useState([]);
-
-  useEffect(() => {
-    let PROJECT_ID = "j1fgz8za";
-    let DATASET = "production";
-    let QUERY = encodeURIComponent('*[_type == "artwork"]');
-
-    let URL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`;
-    fetch(URL)
-      .then((res) => res.json())
-      .then(({ result }) => {
-        if (result.length > 0) {
-          result.forEach((artwork) => {
-            setArtworks((oldArray) => [...oldArray, artwork]);
-          });
-        }
-      })
-      .catch((err) => console.error(err));
-  }, []);
+export default function Gallery(props) {
+  const artworks = props.artworks
 
   return (
-    <>
-      <div className="gallery-container">
-        {artworks.map((artwork) => (
-          <GalleryCard
-            title={artwork.title}
-            imageUrl={artwork.imageUrl}
-            date={artwork.date}
-            medium={artwork.medium}
-          />
-        ))}
-      </div>
-    </>
+    <div className="gallery-container">
+      {artworks.map((artwork) => (
+        <GalleryCard
+          key={artwork.id} // Make sure to provide a unique key
+          title={artwork.title}
+          imageUrl={artwork.imageUrl}
+          date={artwork.date}
+          medium={artwork.medium}
+        />
+      ))}
+    </div>
   );
 }
-
-export default Gallery;
 
 // const handleShuffle = useCallback(() => {
 //      setArtworks((artworks) => [...artworks].sort(() => Math.random() - 0.5));
